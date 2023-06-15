2fd10ecd3c6f57e020e8d94b532e7b0dac090a13
Frederic Vogels <frederic.vogels@ucll.be>
2023-06-15 20:49:20 +0200

    added router

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
 import './index.css';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Root</div>
+  },
+  {
+    path: '/hello',
+    element: <div>Hello</div>
+  }
+]);
 
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
