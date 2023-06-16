a533194da22c20e560b90d9d567237397cbd6b0b
Frederic Vogels <frederic.vogels@ucll.be>
2023-06-16 23:59:05 +0200

    rest related functionality

---
+++ src/rest.ts
@@ -0,0 +1,48 @@
+export interface Section {
+    type: 'section',
+    path: string,
+    tree_path: string[],
+    children: string[],
+}
+
+export interface Explanation {
+    type: 'explanation',
+    path: string,
+    tree_path: string[],
+}
+
+export interface Exercise {
+    type: 'exercise',
+    path: string,
+    tree_path: string[],
+}
+
+export type MaterialNode = Section | Explanation | Exercise;
+
+
+export function isSection(node: MaterialNode) : node is Section
+{
+    return node.type == 'section';
+}
+
+export function isExplanation(node: MaterialNode) : node is Explanation
+{
+    return node.type == 'explanation';
+}
+
+export function isExercise(node: MaterialNode) : node is Exercise
+{
+    return node.type == 'exercise';
+}
+
+export async function fetchNodeData(tree_path: string[]) : Promise<Response>
+{
+    const url = buildNodeUrl(tree_path);
+
+    return await fetch(url);
+}
+
+export function buildNodeUrl(tree_path: string[]) : string
+{
+    return `/api/v1/nodes/${tree_path.join("/")}`;
+}
