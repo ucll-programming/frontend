d24429ccfe19bd9a911f505224671d33257dce24
Frederic Vogels <frederic.vogels@ucll.be>
2023-06-17 21:16:55 +0200

    updated imports

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { LoaderFunctionArgs, RouterProvider, createBrowserRouter } from 'react-router-dom';
 import App from './App';
-import NodeViewer from './components/NodeViewer';
+import NodeViewer from '@/components/NodeViewer';
 import { fetchNodeData } from './rest';
 
 
