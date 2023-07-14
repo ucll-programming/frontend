b287acaf5403314475afdc74d71bed3304315dbe
Frederic Vogels <frederic.vogels@ucll.be>
2023-07-14 07:25:00 +0200

    App shows loading message

--- src/App.tsx
+++ src/App.tsx
@@ -6,34 +6,42 @@ import Sidebar from './components/Sidebar';
 
 function App()
 {
-  const [domain, setDomain] = useState<Domain | null>(null);
+    const [ domain, setDomain ] = useState<Domain | null>( null );
 
-  useEffect(
-    () => {
-      (async () => {
-        const domain = await loadDomain();
+    useEffect(
+        () =>
+        {
+            ( async () =>
+            {
+                const domain = await loadDomain();
 
-        setDomain(domain);
-      })();
-    },
-    []
-  );
-
-  if ( domain )
-  {
-    return (
-      <>
-        <DomainContext.Provider value={domain}>
-          <Sidebar root={domain.root} />
-          <div id="main-view-container">
-            <div id="main-view">
-              <Outlet />
-            </div>
-          </div>
-        </DomainContext.Provider>
-      </>
+                setDomain( domain );
+            } )();
+        },
+        []
     );
-  }
+
+    if ( domain )
+    {
+        return (
+            <DomainContext.Provider value={domain}>
+                <Sidebar root={domain.root} />
+                <div id="main-view-container">
+                    <div id="main-view">
+                        <Outlet />
+                    </div>
+                </div>
+            </DomainContext.Provider>
+        );
+    }
+    else
+    {
+        return (
+            <p>
+                Loading...
+            </p>
+        );
+    }
 }
 
 export default App;
