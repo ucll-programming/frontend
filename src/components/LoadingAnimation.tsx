3f5219aa8de79dc78d3872a6298f25f3a52b33c1
Frederic Vogels <frederic.vogels@ucll.be>
2023-07-14 08:22:14 +0200

    LoadingAnimation delay

--- src/components/LoadingAnimation.tsx
+++ src/components/LoadingAnimation.tsx
@@ -1,14 +1,30 @@
 import ReactLoading from "react-loading";
 import CenterBox from "./CenterBox";
+import React from "react";
 
 
-function LoadingAnimation()
+function LoadingAnimation({ delay = 200 }: { delay?: number })
 {
-    return (
-        <CenterBox>
-            <ReactLoading />
-        </CenterBox>
-    )
+    const [show, setShow] = React.useState<boolean>(false);
+
+    React.useEffect(() => {
+        setTimeout(() => setShow(true), delay);
+    }, [ delay ]);
+
+    if ( show )
+    {
+        return (
+            <CenterBox>
+                <ReactLoading />
+            </CenterBox>
+        )
+    }
+    else
+    {
+        return (
+            <></>
+        );
+    }
 }
 
 export default LoadingAnimation;
