563e3a68a4a533a0b5d1e6d7802b3f572b3da780
Frederic Vogels <frederic.vogels@ucll.be>
2023-07-16 17:08:42 +0200

    removed console.log

--- src/markdown/div.ts
+++ src/markdown/div.ts
@@ -12,8 +12,6 @@ const remarkDiv: Plugin = () => {
                 const data = node.data || (node.data = {});
                 const tagName = 'div';
 
-                console.log(node.attributes);
-
                 data.hName = tagName;
                 data.hProperties = {className: [node.attributes?.class], style: node.attributes?.style};
             }
