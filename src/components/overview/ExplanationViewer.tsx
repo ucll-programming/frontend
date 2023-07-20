9636cbee754b36b90ff7e519146db04240f01bd0
Frederic Vogels <frederic.vogels@ucll.be>
2023-07-20 18:31:32 +0200

    made ExplanationViewer independent of LeafViewer

--- src/components/overview/ExplanationViewer.tsx
+++ src/components/overview/ExplanationViewer.tsx
@@ -1,17 +1,42 @@
 import { Explanation } from '@/domain';
 import NodeSymbolViewer from '@/components/NodeSymbolViewer';
-import LeafViewer from '@/components/overview/LeafViewer';
+import { Link } from 'react-router-dom';
+import { buildPageUrl } from '@/util';
+import { useActiveTreePath } from '@/main';
 
 
 function ExplanationViewer({ explanation }: { explanation: Explanation }): JSX.Element
 {
+    const activeTreePath = useActiveTreePath();
+
     return (
-        <LeafViewer
-            caption={explanation.name}
-            symbol={<NodeSymbolViewer node={explanation} />}
-            classNames={['explanation']}
-            treePath={explanation.treePath} />
+        <div className={determineClassName()}>
+            <h1 className='overview-entry-header'>
+                <Link to={buildPageUrl(explanation.treePath)}>
+                    <span className='overview-entry-header-label'>
+                        {explanation.name}
+                    </span>
+                    <span className='overview-entry-header-symbol'>
+                    <NodeSymbolViewer node={explanation} />
+                    </span>
+                </Link>
+            </h1>
+        </div>
     );
+
+
+    function determineClassName(): string
+    {
+        const isSelected = explanation.treePath.isEqualTo(activeTreePath);
+        const result = [ 'overview-entry', 'explanation' ];
+
+        if ( isSelected )
+        {
+            result.push('selected');
+        }
+
+        return result.join(' ');
+    }
 }
 
 export default ExplanationViewer;
