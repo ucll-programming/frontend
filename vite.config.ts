ab9d7cfaf51b6ae4c7f5033d34b250b17fc03992
Frederic Vogels <frederic.vogels+github@gmail.com>
2023-08-27 19:36:10 +0200

    version injected into html file

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,27 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { viteSingleFile } from "vite-plugin-singlefile"
 import tsconfigPaths from 'vite-tsconfig-paths';
+import { createHtmlPlugin } from 'vite-plugin-html';
+
+
+
 
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), viteSingleFile(), tsconfigPaths()],
+  plugins: [
+    react(),
+    viteSingleFile(),
+    tsconfigPaths(),
+    createHtmlPlugin({
+      minify: true,
+      entry: 'src/main.tsx',
+      template: 'index.html',
+      inject: {
+        data: {
+          version: `<meta name="version" content="${process.env.npm_package_version}">`,
+        }
+      }
+    })
+  ],
 })
